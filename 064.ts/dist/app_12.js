d0350217fefd28598c0fc46c98baded66ad8d003
Dmitry Tinkov <mrfredyon@gmail.com>
2024-02-07 11:46:15 +0300

    sf

--- 064.ts/dist/app_12.js
+++ 064.ts/dist/app_12.js
@@ -1,21 +1,16 @@
 // Создайте переменную str и присвойте ей строковое значение. Используя цикл
 // while, замените все гласные символы в строке на символ *.
-
-const str2: string = "LALALALLALALALAL";
-
-const arr1: string[] = ["a", "e", "o", "u", "i", "y"];
-
-const arr2: string[] = [];
-
-let count: any = 0;
-
+const str2 = "LALALALLALALALAL";
+const arr1 = ["a", "e", "o", "u", "i", "y"];
+const arr2 = [];
+let count = 0;
 while (count < str2.length) {
-  if (arr1.includes(str2[count].toLowerCase())) {
-    arr2.push("*");
-  } else {
-    arr2.push(str2[count]);
-  }
-  count++;
+    if (arr1.includes(str2[count].toLowerCase())) {
+        arr2.push("*");
+    }
+    else {
+        arr2.push(str2[count]);
+    }
+    count++;
 }
-
 console.log(arr2.join(""));
